101d56fb0e6fe0a6933904ad39034d8e086a505c
Shweta <143203479+shwet46@users.noreply.github.com>
2024-12-06 00:56:24 +0530

    .

--- shweta-behera/src/components/Header.jsx
+++ shweta-behera/src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-transparent">
+    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-zinc-900">
       <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,9fr,1fr]">
         {/* Logo */}
         <h1>
